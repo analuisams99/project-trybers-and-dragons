0fdbcf37001069d408a8c7235add927831564e2a
Ana Luisa <ana.luisa.isa@hotmail.com>
2022-04-12 21:55:46 -0300

    Cria Classe Warrior  que estende a classe Archetype

---
+++ src/Archetypes/Warrior.ts
@@ -0,0 +1,21 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+export default class Warrior extends Archetype {
+  private _energyType: EnergyType = 'stamina';
+  private static _createdInstance = 0;
+
+  constructor(name:string) {
+    super(name);
+
+    Warrior._createdInstance += 1;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  static createdArchetypeInstances(): number {
+    return this._createdInstance;
+  }
+}
